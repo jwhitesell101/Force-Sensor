bde10a4d1433bbb93532676d1fa06b568b60cb3d
jwhitesell101 <82953153+jwhitesell101@users.noreply.github.com>
2021-04-22 08:39:31 -0500

    Add files via upload

---
+++ sketch_apr07a_w_french.ino
@@ -0,0 +1,24 @@
+#include <LiquidCrystal.h> //new
+LiquidCrystal lcd(4, 5, 6, 7, 8, 9);
+int FSR_Pin =  A0;
+//analog pin
+void setup() {
+  Serial.begin(9600);
+
+
+}
+
+void loop() {
+  int force;
+  int FSRReading = analogRead(FSR_Pin);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+
+  lcd.print("Force in PA ");
+  lcd.print(FSRReading);
+
+  Serial.println(FSRReading);
+
+
+  delay(1000); //makes output faster or slower
+}
